c3493c428b2fd57c03b2236e18c659258d41e068
jklamert <81494203+jklamert@users.noreply.github.com>
2023-05-21 11:35:46 -0500

    Fix code gen config
    Remove context type from code gen config

--- codegen.ts
+++ codegen.ts
@@ -4,7 +4,6 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 const config: CodegenConfig = {
   overwrite: true,
   schema: "./src/schema.graphql",
-  contextType: "../index#MyContext",
   generates: {
     "src/generated/graphql.ts": {
       plugins: ["typescript", "typescript-resolvers"]
